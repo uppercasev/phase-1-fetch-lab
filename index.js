1d882273cf8aa34adf052b14a4eb255cdbbfdd55
Vi Tran <contact.vitran@gmail.com>
2021-11-17 22:01:40 +1100

    Functions added: fetchBooks, findNthBook, totalPages

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+//Below code uses async/await, which will meet deliverables, but will not pass test, as they are based on using .then() method.
+async function fetchBooks() {
+  const response = await fetch('https://anapioficeandfire.com/api/books');
+  const data = await response.json();
+  console.log(data);
+  return data;
 }
 
 function renderBooks(books) {
@@ -12,6 +15,27 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+//Below suggested deliverables from Canvas module:
+//1. Find the 5th book in the series
+function findNthBook(books, nth) {
+  console.log(`Book number ${nth} of the series is called "${books[nth - 1].name}".`);
+}
+
+// 2. Find the 1031st character in the series
+
+// 3. Find the total number of pages of all the books
+function totalPages(books) {
+  //get value of numberOfPages of each book and put into new array using .map() method
+  const pagesArray = books.map((book) => book.numberOfPages);
+  //use .reduce() method to add all numbers together
+  const reducerFunction = (previousValue, currentValue) => previousValue + currentValue;
+  const sumPages = pagesArray.reduce(reducerFunction);
+  console.log(`The total number of pages in this series is ${sumPages}.`);
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+  const booksList = await fetchBooks();
+  renderBooks(booksList);
+  findNthBook(booksList, 5);
+  totalPages(booksList);
 });
